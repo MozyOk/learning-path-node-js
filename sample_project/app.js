1af62eb2578bced9f829ef61f55308b09d9a91f7
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-09-27 21:40:20 +0900

    first commit

---
+++ sample_project/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+
+app.get("/", function(request,response){
+  response.send("<h1 style='text-align:center'>Hey friends, welcome to my app</h1>");
+})
+
+app.listen(3000, function(error){
+  if(error == true){
+    console.log("some error occured");
+  }else{
+    console.log("listening on localhost:3000");
+  }
+})
